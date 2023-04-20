de3d245aa226c5e34106a667a5845ec3f989ce22
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-04-20 16:44:23 +0100

    Initialisation of redux system

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,19 @@
+'use client';
+
+import SearchAppBar from "./components/SearchAppBar";
+import { Provider } from 'react-redux';
+import store from '../store';
+
 const Home = () => {
   return (
-    <main style={{padding: 30, background: '#B22222', height: '100vh'}}>
-      <h1>Search Results</h1>
-    </main>
+    <Provider store={store}>
+      <main>
+        <SearchAppBar />
+        <div style={{padding: 30, background: '#B22222', height: '100vh'}}>
+          <h1>Search Results</h1>
+        </div>
+      </main>
+    </Provider>
   );
 };
 
